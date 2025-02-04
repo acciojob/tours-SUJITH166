592cea56b9e3d8b477dca17251e44a144aa6626a
SUJITH166 <85979919+SUJITH166@users.noreply.github.com>
2025-02-04 20:41:53 +0530

    Update Tour.js

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-const Tour = ({ tour, onDelete }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
-  const { id, name, info, image, price } = tour;
+// const Tour = ({ tour, onDelete }) => {
+//   const [isExpanded, setIsExpanded] = useState(false);
+//   const { id, name, info, image, price } = tour;
 
-  const toggleInfo = () => {
-    setIsExpanded(!isExpanded);
-  };
+//   const toggleInfo = () => {
+//     setIsExpanded(!isExpanded);
+//   };
 
-  return (
-    <div className="tour">
-      <img src={image} alt={name} />
-      <h3>{name}</h3>
-      <p id={tour-item-para-${id}}>{isExpanded ? info : ${info.substring(0, 200)}...}</p>
-      <button id={see-more-${id}} onClick={toggleInfo}>
-  {isExpanded ? "Show less" : "See more"}
-</button>
+//   return (
+//     <div className="tour">
+//       <img src={image} alt={name} />
+//       <h3>{name}</h3>
+//       <p id={tour-item-para-${id}}>{isExpanded ? info : ${info.substring(0, 200)}...}</p>
+//       <button id={see-more-${id}} onClick={toggleInfo}>
+//   {isExpanded ? "Show less" : "See more"}
+// </button>
 
-      <p>${price}</p>
-      <button id={delete-btn-${id}} onClick={() => onDelete(id)}>Delete Tour</button>
-    </div>
-  );
-};
+//       <p>${price}</p>
+//       <button id={delete-btn-${id}} onClick={() => onDelete(id)}>Delete Tour</button>
+//     </div>
+//   );
+// };
 
-export default Tour;
+// export default Tour;
